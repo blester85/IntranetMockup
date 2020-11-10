816594b7f95fd40990f5c2f449cd64fd07bdca3e
Brandon Lester <brandon.lester@brevardclerk.us>
2020-11-10 11:08:28 -0500

    Finished tab arrow & dropdown functionality

--- js/main.js
+++ js/main.js
@@ -1,89 +1,199 @@
+$('.col-6>.list-group-flush>.list-group-item').hover(function() {
+  $(this)
+    .stop()
+    .animate({
+      fontSize: '1.1em'
+    }, 100);
+}, function() {
+  $(this)
+    .stop()
+    .animate({
+      fontSize: '1em'
+    }, 200);
+});
+
+$.changeInfoArrowDirection = function(direction) {
+  if (direction === 'down') {
+    $('#infoArrow').removeClass('fa-caret-right').addClass('fa-caret-down');;
+  } else if (direction === 'right') {
+    $('#infoArrow').removeClass('fa-caret-down').addClass('fa-caret-right');;
+  }
+};
+
+$.changeProceduresArrowDirection = function(direction) {
+  if (direction === 'down') {
+    $('#proceduresArrow').removeClass('fa-caret-right').addClass('fa-caret-down');;
+  } else if (direction === 'right') {
+    $('#proceduresArrow').removeClass('fa-caret-down').addClass('fa-caret-right');;
+  }
+}
+
+$.changeHRArrowDirection = function(direction) {
+  if (direction === 'down') {
+    $('#hrArrow').removeClass('fa-caret-right').addClass('fa-caret-down');;
+  } else if (direction === 'right') {
+    $('#hrArrow').removeClass('fa-caret-down').addClass('fa-caret-right');;
+  }
+}
+
+$.changeMoneyArrowDirection = function(direction) {
+  if (direction === 'down') {
+    $('#moneyArrow').removeClass('fa-caret-right').addClass('fa-caret-down');;
+  } else if (direction === 'right') {
+    $('#moneyArrow').removeClass('fa-caret-down').addClass('fa-caret-right');;
+  }
+}
+
+$.changePersonnelArrowDirection = function(direction) {
+  if (direction === 'down') {
+    $('#personnelArrow').removeClass('fa-caret-right').addClass('fa-caret-down');;
+  } else if (direction === 'right') {
+    $('#personnelArrow').removeClass('fa-caret-down').addClass('fa-caret-right');;
+  }
+}
+
+$.changeFormsArrowDirection = function(direction) {
+  if (direction === 'down') {
+    $('#formsArrow').removeClass('fa-caret-right').addClass('fa-caret-down');;
+  } else if (direction === 'right') {
+    $('#formsArrow').removeClass('fa-caret-down').addClass('fa-caret-right');
+  }
+}
+
+$.infoPanelStatus = function(status) {
+  if (status === 'open') {
+    $('#info').removeClass('hidden').addClass('show');
+  } else if (status === 'close') {
+    $('#info').removeClass('show').addClass('hidden');
+  }
+}
+
+$.proceduresPanelStatus = function(status) {
+  if (status === 'open') {
+    $('#procedures').removeClass('hidden').addClass('show');
+  } else if (status === 'close') {
+    $('#procedures').removeClass('show').addClass('hidden');
+  }
+}
+
+$.hrPanelStatus = function(status) {
+  if (status === 'open') {
+    $('#hr').removeClass('hidden').addClass('show');
+  } else if (status === 'close') {
+    $('#hr').removeClass('show').addClass('hidden');
+  }
+}
+
+$.moneyPanelStatus = function(status) {
+  if (status === 'open') {
+    $('#money').removeClass('hidden').addClass('show');
+  } else if (status === 'close') {
+    $('#money').removeClass('show').addClass('hidden');
+  }
+}
+
+$.personnelPanelStatus = function(status) {
+  if (status === 'open') {
+    $('#personnel').removeClass('hidden').addClass('show');
+  } else if (status === 'close') {
+    $('#personnel').removeClass('show').addClass('hidden');
+  }
+}
+
+$.formsPanelStatus = function(status) {
+  if (status === 'open') {
+    $('#forms').removeClass('hidden').addClass('show');
+  } else if (status === 'close') {
+    $('#forms').removeClass('show').addClass('hidden');
+  }
+}
+
 $('#infoHeading').click(function() {
   if ($('#infoHeading>a>i#infoArrow').hasClass('fa-caret-right')) {
-    $('#info').removeClass('hidden');
-    $('#info').addClass('show');
-
-    $('#infoArrow').removeClass('fa-caret-right');
-    $('#infoArrow').addClass('fa-caret-down');
-
-    $('#proceduresArrow').removeClass('fa-caret-down');
-    $('#proceduresArrow').addClass('fa-caret-right');
-
-    $('#hrArrow').removeClass('fa-caret-down');
-    $('#hrArrow').addClass('fa-caret-right');
-
-    $('#moneyArrow').removeClass('fa-caret-down');
-    $('#moneyArrow').addClass('fa-caret-right');
-
-    $('#personnelArrow').removeClass('fa-caret-down');
-    $('#personnelArrow').addClass('fa-caret-right');
-
-    $('#formsArrow').removeClass('fa-caret-down');
-    $('#formsArrow').addClass('fa-caret-right');
+    $.infoPanelStatus('open');
+    $.changeInfoArrowDirection('down');
+    $.changeProceduresArrowDirection('right');
+    $.changeHRArrowDirection('right');
+    $.changeMoneyArrowDirection('right');
+    $.changePersonnelArrowDirection('right');
+    $.changeFormsArrowDirection('right');
   } else {
-    $('#info').removeClass('show');
-    $('#info').addClass('hidden');
-    $('#infoArrow').removeClass('fa-caret-down');
-    $('#infoArrow').addClass('fa-caret-right');
+    $.infoPanelStatus('close');
+    $.changeInfoArrowDirection('right');
   }
 });
 
 $('#proceduresHeading').click(function() {
   if ($('#proceduresHeading>a>i#proceduresArrow').hasClass('fa-caret-right')) {
-    $('#procedures').removeClass('hidden');
-    $('#procedures').addClass('show');
-
-    $('#proceduresArrow').removeClass('fa-caret-right');
-    $('#proceduresArrow').addClass('fa-caret-down');
-
-    $('#infoArrow').removeClass('fa-caret-down');
-    $('#infoArrow').addClass('fa-caret-right');
-
-    $('#hrArrow').removeClass('fa-caret-down');
-    $('#hrArrow').addClass('fa-caret-right');
-
-    $('#moneyArrow').removeClass('fa-caret-down');
-    $('#moneyArrow').addClass('fa-caret-right');
-
-    $('#personnelArrow').removeClass('fa-caret-down');
-    $('#personnelArrow').addClass('fa-caret-right');
-
-    $('#formsArrow').removeClass('fa-caret-down');
-    $('#formsArrow').addClass('fa-caret-right');
+    $.proceduresPanelStatus('open');
+    $.changeProceduresArrowDirection('down');
+    $.changeInfoArrowDirection('right');
+    $.changeHRArrowDirection('right');
+    $.changeMoneyArrowDirection('right');
+    $.changePersonnelArrowDirection('right');
+    $.changeFormsArrowDirection('right');
   } else {
-    $('#procedures').removeClass('show');
-    $('#procedures').addClass('hidden');
-    $('#proceduresArrow').removeClass('fa-caret-down');
-    $('#proceduresArrow').addClass('fa-caret-right');
+    $.proceduresPanelStatus('close');
+    $.changeProceduresArrowDirection('right');
   }
 });
 
 $('#hrHeading').click(function() {
   if ($('#hrHeading>a>i#hrArrow').hasClass('fa-caret-right')) {
-    $('#hr').removeClass('hidden');
-    $('#hr').addClass('show');
-
-    $('#hrArrow').removeClass('fa-caret-right');
-    $('#hrArrow').addClass('fa-caret-down');
-
-    $('#infoArrow').removeClass('fa-caret-down');
-    $('#infoArrow').addClass('fa-caret-right');
-
-    $('#proceduresArrow').removeClass('fa-caret-down');
-    $('#proceduresArrow').addClass('fa-caret-right');
-
-    $('#moneyArrow').removeClass('fa-caret-down');
-    $('#moneyArrow').addClass('fa-caret-right');
-
-    $('#personnelArrow').removeClass('fa-caret-down');
-    $('#personnelArrow').addClass('fa-caret-right');
-
-    $('#formsArrow').removeClass('fa-caret-down');
-    $('#formsArrow').addClass('fa-caret-right');
+    $.hrPanelStatus('open');
+    $.changeHRArrowDirection('down');
+    $.changeInfoArrowDirection('right');
+    $.changeProceduresArrowDirection('right');
+    $.changeMoneyArrowDirection('right');
+    $.changePersonnelArrowDirection('right');
+    $.changeFormsArrowDirection('right');
   } else {
-    $('#hr').removeClass('show');
-    $('#hr').addClass('hidden');
-    $('#hrArrow').removeClass('fa-caret-down');
-    $('#hrArrow').addClass('fa-caret-right');
+    $.hrPanelStatus('close');
+    $.changeHRArrowDirection('right');
+  }
+});
+
+$('#moneyHeading').click(function() {
+  if ($('#moneyHeading>a>i#moneyArrow').hasClass('fa-caret-right')) {
+    $.moneyPanelStatus('open');
+    $.changeMoneyArrowDirection('down');
+    $.changeInfoArrowDirection('right');
+    $.changeProceduresArrowDirection('right');
+    $.changeHRArrowDirection('right');
+    $.changePersonnelArrowDirection('right');
+    $.changeFormsArrowDirection('right');
+  } else {
+    $.moneyPanelStatus('close');
+    $.changeMoneyArrowDirection('right');
+  }
+});
+
+$('#personnelHeading').click(function() {
+  if ($('#personnelHeading>a>i#personnelArrow').hasClass('fa-caret-right')) {
+    $.personnelPanelStatus('open');
+    $.changePersonnelArrowDirection('down');
+    $.changeInfoArrowDirection('right');
+    $.changeProceduresArrowDirection('right');
+    $.changeHRArrowDirection('right');
+    $.changeMoneyArrowDirection('right');
+    $.changeFormsArrowDirection('right');
+  } else {
+    $.personnelPanelStatus('close');
+    $.changePersonnelArrowDirection('right');
+  }
+});
+
+$('#formsHeading').click(function() {
+  if ($('#formsHeading>a>i#formsArrow').hasClass('fa-caret-right')) {
+    $.formsPanelStatus('open');
+    $.changeFormsArrowDirection('down');
+    $.changeInfoArrowDirection('right');
+    $.changeProceduresArrowDirection('right');
+    $.changeHRArrowDirection('right');
+    $.changeMoneyArrowDirection('right');
+    $.changePersonnelArrowDirection('right');
+  } else {
+    $.formsPanelStatus('close');
+    $.changeFormsArrowDirection('right');
   }
 });
